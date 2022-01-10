409edd7568afb8acffc6e9f89063ddb9ea1ecd62
yasir19 <yasir.habboo@gmail.com>
2022-01-10 00:14:07 -0600

    adding mock data to replace potion for testing and refactor player constructor function

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,3 +1,6 @@
+const Potion =require ('../lib/Potion');
+jest.mock('../lib/Potion');
+console.log(new Potion());
 const Player = require ('../lib/Player');
 test ('create a player object',() => {
     const player = new Player('Yasir');
@@ -5,4 +8,6 @@ test ('create a player object',() => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)]));
 });
