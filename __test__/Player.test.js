4a2bd44337f6775fec671859c582fd14acf24d72
yasir19 <yasir.habboo@gmail.com>
2022-01-09 23:44:55 -0600

    creating a player constructor function to pass the test

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,10 +1,8 @@
-const { toBindingIdentifierName } = require('@babel/types');
-const { expect } = require('@jest/globals');
 const Player = require ('../lib/Player');
 test ('create a player object',() => {
     const player = new Player('Yasir');
     expect(player.name).toBe('Yasir');
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
-    expect(player.potion).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
 });
